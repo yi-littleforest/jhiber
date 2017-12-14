0ac23e29fae13483b69adc804cbaf1e85cdce1a9
yi <yi@yi-HP-3120>
2017-12-14 16:45:58 +0000

    first commit

---
+++ src/main/java/com/s1/jhiber/domain/Movie.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.s1.jhiber.domain;
+
+/**
+ *
+ * @author yi
+ */
+public class Movie {
+    private int id = 0;
+
+    private String  title = null;
+
+    private String  synopsis = null;
+
+    private String  director = null;
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the synopsis
+     */
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    /**
+     * @param synopsis the synopsis to set
+     */
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    /**
+     * @return the director
+     */
+    public String getDirector() {
+        return director;
+    }
+
+    /**
+     * @param director the director to set
+     */
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    
+}
